05b52a0405095a09cbabee86880a918a4e74fc8e
Yuyang Zhang <695441493@qq.com>
2016-11-03 23:08:41 +0800

    add test for login

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 import pytest
+from app.models import AppUser
 
 
 @pytest.mark.django_db
 def test_user(client):
-    response = client.post('/api/user/login/')
+    url = '/api/user/login/'
+    response = client.get(url)
     assert response.status_code == 200
+    assert response.json()['code'] == 501
+    assert response.json()['message'] == 'Not support this method'
+
+    response = client.post(url)
+    assert response.status_code == 200
+    assert response.json()['code'] == 402
+    assert response.json()['message'] == 'Username and Password not match'
+
+    user = AppUser.objects.create_user(username='test', password='test')
+    user.save()
+
+    response = client.post(url, data={'username': 'test', 'password': 'test'})
+    assert response.status_code == 200
+    assert response.json()['username'] == 'test'
+    assert response.json()['user_id'] == user.pk
