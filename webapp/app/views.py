a02212bb6ae4f1ecbf72952676f8e188f2a6e29f
lc4t <lc4t0.0@gmail.com>
2016-11-23 03:42:32 +0800

    more api: list add id

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
+from app.models import Label, Place, Guide
+from hashlib import md5
 import json
 import re
 
@@ -11,6 +13,10 @@ LOGOUT_OK_CODE = 201
 LOGOUT_OK = 'Logout success'
 REG_OK_CODE = 202
 REG_OK = 'Regist success'
+QUERY_OK_CODE = 203
+QUERY_OK = ''
+ADD_OK_CODE = 203
+ADD_OK = 'Add success'
 HAD_LOGIN_CODE = 301
 HAD_LOGIN = 'Had logined'
 NOT_LOGIN_CODE = 301
@@ -33,6 +39,18 @@ EMAIL_EX_CODE = 422
 EMAIL_EX = 'Email has already regist'
 UNAME_EX_CODE = 423
 UNAME_EX = 'Username has already regist'
+NAME_EX_CODE = 424
+NAME_EX = 'This name is already exists'
+KEY_ERR_CODE = 425
+KEY_ERR = 'The Key Error'
+ID_ERR_CODE = 426
+ID_ERR = 'The ID Error'
+TITLE_ERR_CODE = 427
+TITLE_ERR = 'The Title Error'
+PLACE_ERR_CODE = 428
+PLACE_ERR = 'The Place Error'
+LABEL_ERR_CODE = 429
+LABEL_ERR = 'The Label Error'
 INVALIED_CODE = 501
 INVALIED = 'Not support this method'
 UN_ERROR_CODE = 502
@@ -119,6 +137,7 @@ def user_register(request):
 
                 app_user = get_user_model()
                 try:
+                    birthday = birthday.replace('.', '-').replace('/', '-')
                     user = app_user.objects.create_user(username=username,
                                                         password=password,
                                                         email=email,
@@ -132,3 +151,157 @@ def user_register(request):
                     data = JSON(code=UN_ERROR_CODE, status=False,
                                 message=UN_ERROR)
     return HttpResponse(data, content_type="application/json")
+
+
+def guide_add(request):
+    if request.user.is_authenticated():
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            content = request.POST.get('content')
+            place = request.POST.get('place')
+            label = request.POST.getlist('label[]')
+            start_time = request.POST.get('start_time')
+            end_time = request.POST.get('end_time')
+
+            if len(title) == 0:
+                data = JSON(code=TITLE_ERR_CODE, status=False,
+                            message=TITLE_ERR)
+            elif len(place) == 0:
+                data = JSON(code=PLACE_ERR_CODE, status=False,
+                            message=PLACE_ERR)
+            elif re.match(r'(\d{4}([-/\.])\d{2}\2\d{2})', start_time) is None:
+                data = JSON(code=DATE_ERR_CODE, status=False, message=DATE_ERR)
+            elif re.match(r'(\d{4}([-/\.])\d{2}\2\d{2})', end_time) is None:
+                data = JSON(code=DATE_ERR_CODE, status=False, message=DATE_ERR)
+            elif start_time > end_time:
+                data = JSON(code=DATE_ERR_CODE, status=False, message=DATE_ERR)
+            elif not Place.objects.filter(id=place):
+                data = JSON(code=PLACE_ERR_CODE, status=False,
+                            message=PLACE_ERR)
+            else:
+                label = Label.objects.filter(id__in=label)
+                a = Guide(name=title, user=request.user,
+                          place=Place.objects.get(id=place), content=content,
+                          start_time=start_time, end_time=end_time)
+                a.save()
+                a.label.add(*label)
+                data = JSON(code=ADD_OK_CODE, status=True, message=ADD_OK)
+        else:
+            data = JSON(code=INVALIED_CODE, status=False, message=INVALIED)
+    else:
+        data = JSON(code=NOT_LOGIN_CODE, status=False, message=NOT_LOGIN)
+    return HttpResponse(data, content_type="application/json")
+
+
+def guide_id(request):
+    pass
+
+
+def guide_list(request):
+    if request.user.is_authenticated():
+        if request.method == 'POST':
+            start = int(request.POST.get('start'))
+            offset = int(request.POST.get('offset'))
+            try:
+                ans = Guide.objects.order_by('-submit')[start:start + offset]
+            except IndexError:
+                ans = []
+            result = []
+            for i in ans:
+                labels = []
+                for l in i.label.all():
+                    labels.append(l.name)
+                m = md5()
+                m.update(i.user.email.encode())
+                img = 'http://gravatar.eqoe.cn/avatar/%s?size=48&default=identicon&rating=pg' % (m.hexdigest())
+                # _ = JSON(id=i.id, username=i.user.username, title=i.name,
+                #          place=i.place.name, pageview=i.pageview, label=label,
+                #          img=img)
+                _ = {'id': i.id, 'username': i.user.username, 'title': i.name,
+                     'place': i.place.name, 'pageview': i.pageview,
+                     'labels': labels, 'img': img}
+                result.append(_)
+            data = JSON(code=QUERY_OK_CODE, status=True, message=result)
+        else:
+            data = JSON(code=INVALIED_CODE, status=False, message=INVALIED)
+    else:
+        data = JSON(code=NOT_LOGIN_CODE, status=False, message=NOT_LOGIN)
+    return HttpResponse(data, content_type="application/json")
+
+
+def question_add(request):
+    pass
+
+
+def question_id(request, id):
+    pass
+
+
+def question_list(request):
+    pass
+
+
+def __id(request, _id, model):
+    if request.user.is_authenticated():
+        try:
+            ans = model.objects.filter(id=_id)[0].name
+            data = JSON(code=QUERY_OK_CODE, status=True, message=ans)
+        except IndexError:
+            data = JSON(code=ID_ERR_CODE, status=False, message=ID_ERR)
+    else:
+        data = JSON(code=NOT_LOGIN_CODE, status=False, message=NOT_LOGIN)
+    return HttpResponse(data, content_type="application/json")
+
+
+def label_id(request, _id):
+    return __id(request, _id, Label)
+
+
+def place_id(request, _id):
+    return __id(request, _id, Place)
+
+
+def __list(request, model):
+    if request.user.is_authenticated():
+        ans = list(model.objects.values('id', 'name'))
+        data = JSON(code=QUERY_OK_CODE, status=True, message=ans)
+    else:
+        data = JSON(code=NOT_LOGIN_CODE, status=False, message=NOT_LOGIN)
+    return HttpResponse(data, content_type="application/json")
+
+
+def place_list(request):
+    return __list(request, Place)
+
+
+def label_list(request):
+    return __list(request, Label)
+
+
+def __add(request, model):
+    if request.user.is_authenticated():
+        if request.method == 'POST':
+            if 'name' in request.POST:
+                name = request.POST.get('name')
+                if model.objects.filter(name=name):
+                    data = JSON(code=NAME_EX_CODE, status=False,
+                                message=NAME_EX)
+                else:
+                    add = model(name=name)
+                    add.save()
+                    data = JSON(code=ADD_OK_CODE, status=True, message=ADD_OK)
+            else:
+                data = JSON(code=KEY_ERR_CODE, status=False, message=KEY_ERR)
+        else:
+            data = JSON(code=INVALIED_CODE, status=False, message=INVALIED)
+    else:
+        data = JSON(code=NOT_LOGIN_CODE, status=False, message=NOT_LOGIN)
+    return HttpResponse(data, content_type="application/json")
+
+
+def label_add(request):
+    __add(request, Label)
+
+
+def place_add(request):
+    __add(request, Place)
