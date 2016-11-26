f08d9f0b2cfaa5f628526179d0422fab04c17dd9
Yuyang Zhang <695441493@qq.com>
2016-11-26 21:50:20 +0800

    add test for register

--- webapp/tests/test_user.py
+++ webapp/tests/test_user.py
@@ -5,7 +5,7 @@ import json
 
 
 @pytest.mark.django_db
-def test_user(client):
+def test_user_login(client):
     url = '/api/user/login/'
     response = client.get(url)
     assert response.status_code == 200
@@ -25,3 +25,69 @@ def test_user(client):
     response = json.loads(response.json()['message'])
     assert response['username'] == 'test'
     assert response['user_id'] == user.pk
+
+
+@pytest.mark.django_db
+def test_user_register(client):
+    url = '/api/user/register/'
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.json()['code'] == 501
+    assert response.json()['message'] == 'Not support this method'
+
+    user = dict(username='test_register',
+                password='test',
+                email='test@test.com',
+                phone='13400000000',
+                gender='0',
+                birthday='2000.01.01')
+
+    user['birthday'] = 'error'
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 411
+    assert response.json()['message'] == 'Datetime is not allow'
+    user['birthday'] = '2000.01.01'
+
+    user['gender'] = 'error'
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 412
+    assert response.json()['message'] == 'Gender is not allow'
+    user['gender'] = '0'
+
+    user['phone'] = 'error'
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 413
+    assert response.json()['message'] == 'Phone num is not allow'
+    user['phone'] = '13400000000'
+
+    user['email'] = 'error'
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 414
+    assert response.json()['message'] == 'Email is not allow'
+    user['email'] = 'test@test.com'
+
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 202
+    assert json.loads(response.json()['message'])['username'] == 'test_register'
+
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 421
+    assert response.json()['message'] == 'Phone has already regist'
+
+    user['phone'] = '13400000001'
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 422
+    assert response.json()['message'] == 'Email has already regist'
+
+    user['email'] = 'test@diff.com'
+    response = client.post(url, user)
+    assert response.status_code == 200
+    assert response.json()['code'] == 423
+    assert response.json()['message'] == 'Username has already regist'
